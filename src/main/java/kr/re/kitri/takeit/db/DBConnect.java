882788dc271512dba092be75aa5ae57b80589648
horiJY <45135200+horiJY@users.noreply.github.com>
2021-08-06 09:10:02 +0900

    dbconfig변경,
    localhostdb에서 개발 후 진행예정

--- src/main/java/kr/re/kitri/takeit/db/DBConnect.java
+++ src/main/java/kr/re/kitri/takeit/db/DBConnect.java
@@ -10,12 +10,13 @@ public class DBConnect {
     private static Connection conn;
 
     public static Connection getInstance() {
-	//
 	String user = "web_mini";
-	//
 	String password = "web_mini";
-	// url
-	String url = "jdbc:oracle:thin:@192.168.0.209:1521:xe";
+//	조장DB
+//	String url = "jdbc:oracle:thin:@192.168.0.209:1521:xe";
+//	개발은 로컬에서
+	String url = "jdbc:oracle:thin:@localhost:1521:xe";
+
 	try {
 	    if (conn != null && !conn.isClosed()) {
 		return conn;
