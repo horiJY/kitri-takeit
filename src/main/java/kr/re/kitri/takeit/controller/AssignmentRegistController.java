47b89f0deee76e23d6f8ce3656624f4c9175d762
djkim1013 <dongjukim1013@gmail.com>
2021-08-17 11:59:54 +0900

    assignment controller start

--- src/main/java/kr/re/kitri/takeit/controller/AssignmentRegistController.java
+++ src/main/java/kr/re/kitri/takeit/controller/AssignmentRegistController.java
@@ -1,11 +1,25 @@
 package controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import dao.ScheduleDAO;
+import dao.UserDAO;
+import vo.AssignmentVO;
+import vo.ClassVO;
+import vo.ScheduleVO;
 
 @WebServlet("/assignment/regist")
 public class AssignmentRegistController extends HttpServlet {
@@ -15,8 +29,46 @@ public class AssignmentRegistController extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//수강 create
+		Gson gson = new Gson();
+		JsonObject sJson = gson.fromJson(request.getParameter("schedule"),JsonObject.class);
+		JsonArray sJarr = sJson.get("classSchedule").getAsJsonArray();
+		List<ScheduleVO> slist = new ArrayList<ScheduleVO>();
+		for(int i=0;i<sJarr.size();i++) {
+			ScheduleVO svo = new Gson().fromJson(sJarr.get(i),ScheduleVO.class);
+			slist.add(svo);
+		}
+		HttpSession session = request.getSession();
+		String userId = (String)session.getAttribute("takeit-userid");
 		
+		//스케쥴 중복 체크
+		int result = 0;
+		ScheduleDAO sdao = new ScheduleDAO();
+		for(int i=0;i<slist.size();i++){
+			String stime = slist.get(i).getStartTime();
+			String etime = slist.get(i).getEndTime();
+			result = sdao.selectScheduleOverlap(userId,stime,etime);
+			if(result>0){
+				//중복 시 해당 마이페이지>수강/강의 내역으로 이동
+				response.getWriter().println("OVERLAP");
+				return;
+			}
+		}
+		
+		ClassVO cvo = gson.fromJson(request.getParameter("class"),ClassVO.class);
+		
+		//포인트 잔액 조회
+		UserDAO udao = new UserDAO();
+		int point = udao.selectPoint(userId);
+		if(point < 0) {
+			//포인트 조회 실패
+			
+		}else if (point < cvo.getPrice()) {
+			//포인트 부족시 결제로 이동
+		}
+
+		//등록 조건 충족
+		AssignmentVO avo = new AssignmentVO();
+		avo.setClassId(cvo.getClassId());
 	}
 
 }
--- src/main/java/kr/re/kitri/takeit/dao/UserDAO.java
+++ src/main/java/kr/re/kitri/takeit/dao/UserDAO.java
@@ -1,7 +1,61 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import db.DBConnect;
+import vo.ScheduleVO;
+
 public class UserDAO {
 	//closeAll
-	
+	public void closeAll(Connection conn, PreparedStatement pstmt, Statement stmt, ResultSet rs) {
+		try {
+			if (rs != null && !rs.isClosed()) {
+				rs.close();
+			}
+			if (stmt != null && !stmt.isClosed()) {
+				stmt.close();
+			}
+			if (pstmt != null && !pstmt.isClosed()) {
+				pstmt.close();
+			}
+			if (conn != null && !conn.isClosed()) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 	//login page -> Select
+	
+	//잔여 포인트 조회
+	public int selectPoint(String userid) {
+		//db 접속
+		Connection conn = DBConnect.getInstance();
+		//sql 작성
+		String sql = " SELECT POINT FROM WEBUSER WHERE USERID = '"+userid+"' ";
+		Statement stmt = null;
+		ResultSet rs = null;
+		int point = -1;
+		try {
+			//쿼리 설정
+			stmt = conn.createStatement();
+			//쿼리 실행
+			rs = stmt.executeQuery(sql);
+			if(rs.next()) {
+				point = rs.getInt("POINT");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			closeAll(conn,null,stmt,null);
+		}
+		return point;
+	}
+	
 }
