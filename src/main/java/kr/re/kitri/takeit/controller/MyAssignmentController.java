d13311ce3cd04ba474344991845567c00006908f
horiJY <45135200+horiJY@users.noreply.github.com>
2021-08-19 09:51:15 +0900

    .settings 파일 추가

--- src/main/java/kr/re/kitri/takeit/controller/MyAssignmentController.java
+++ src/main/java/kr/re/kitri/takeit/controller/MyAssignmentController.java
@@ -1,8 +1,6 @@
 package controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,48 +9,42 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-
-import dao.ClassDAO;
-import vo.ClassVO;
-
-
 @WebServlet("/myassignment")
 public class MyAssignmentController extends HttpServlet {
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		doPost(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		String id = (String) session.getAttribute("takeit-userid");
-		
-		ClassDAO cdao = new ClassDAO();
-		List<ClassVO> clist = cdao.selectFavoriteClass(id);
-		
-		JsonArray jsonArr = new JsonArray();
-		
-		for(ClassVO cvo : clist) {
-			JsonObject json = new JsonObject();
-			json.addProperty("className", cvo.getClassName());
-			json.addProperty("creater", cvo.getCreater());
-			json.addProperty("classType", cvo.getClassType());
-			json.addProperty("recommend", cvo.getRecommend());
-			json.addProperty("category", cvo.getCategory());
-			
-			jsonArr.add(json);
-		}
-		
-		System.out.println(jsonArr);
-		Gson gson = new Gson();
-		String jsonResponse = gson.toJson(jsonArr);
-		
-		response.setContentType("text/html; charset=utf-8");
-		PrintWriter out = response.getWriter();
-		out.print(jsonResponse);
+
+		// ClassDAO cdao = new ClassDAO();
+		// List<ClassVO> clist = cdao.selectFavoriteClass(id);
+		//
+		// JsonArray jsonArr = new JsonArray();
+		//
+		// for(ClassVO cvo : clist) {
+		// JsonObject json = new JsonObject();
+		// json.addProperty("className", cvo.getClassName());
+		// json.addProperty("creater", cvo.getCreater());
+		// json.addProperty("classType", cvo.getClassType());
+		// json.addProperty("recommend", cvo.getRecommend());
+		// json.addProperty("category", cvo.getCategory());
+		//
+		// jsonArr.add(json);
+		// }
+		//
+		// System.out.println(jsonArr);
+		// Gson gson = new Gson();
+		// String jsonResponse = gson.toJson(jsonArr);
+		//
+		// response.setContentType("text/html; charset=utf-8");
+		// PrintWriter out = response.getWriter();
+		// out.print(jsonResponse);
 
 	}
 
--- src/main/java/kr/re/kitri/takeit/controller/MyFavoriteController.java
+++ src/main/java/kr/re/kitri/takeit/controller/MyFavoriteController.java
@@ -1,12 +1,7 @@
 package controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,48 +9,41 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-
-import dao.ClassDAO;
-import vo.ClassVO;
-
-
 @WebServlet("/myfavorite")
 public class MyFavoriteController extends HttpServlet {
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		doPost(request, response);
 	}
 
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		HttpSession session = request.getSession();
 		String id = (String) session.getAttribute("takeit-userid");
-		
-		ClassDAO cdao = new ClassDAO();
-		List<ClassVO> clist = cdao.selectFavoriteClass(id);
-		JsonArray jsonArr = new JsonArray();
-		
-		for(ClassVO cvo : clist) {
-			
-			JsonObject json = new JsonObject();
-			json.addProperty("classId", cvo.getClassId());
-			json.addProperty("className", cvo.getClassName());
-			json.addProperty("creater", cvo.getCreater());
-			json.addProperty("favorite", cvo.getFavorite());
-			json.addProperty("openDate", String.valueOf(cvo.getOpenDate()));
-			
-			jsonArr.add(json);
-		}
-		Gson gson = new Gson();
-		String jsonResponse = gson.toJson(jsonArr);
-		
-		response.setContentType("text/html; charset=utf-8");
-		PrintWriter out = response.getWriter();
-		out.print(jsonResponse);
+
+		// ClassDAO cdao = new ClassDAO();
+		// List<ClassVO> clist = cdao.selectFavoriteClass(id);
+		// JsonArray jsonArr = new JsonArray();
+		//
+		// for(ClassVO cvo : clist) {
+		//
+		// JsonObject json = new JsonObject();
+		// json.addProperty("classId", cvo.getClassId());
+		// json.addProperty("className", cvo.getClassName());
+		// json.addProperty("creater", cvo.getCreater());
+		// json.addProperty("favorite", cvo.getFavorite());
+		// json.addProperty("openDate", String.valueOf(cvo.getOpenDate()));
+		//
+		// jsonArr.add(json);
+		// }
+		// Gson gson = new Gson();
+		// String jsonResponse = gson.toJson(jsonArr);
+		//
+		// response.setContentType("text/html; charset=utf-8");
+		// PrintWriter out = response.getWriter();
+		// out.print(jsonResponse);
 	}
 
 }
