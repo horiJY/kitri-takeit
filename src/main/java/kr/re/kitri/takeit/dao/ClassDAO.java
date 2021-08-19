e038147379db2938faaf9b02dbfb563277b73a43
horiJY <45135200+horiJY@users.noreply.github.com>
2021-08-19 09:36:34 +0900

    Class getter setter 카멜케이스 수정

--- src/main/java/kr/re/kitri/takeit/dao/ClassDAO.java
+++ src/main/java/kr/re/kitri/takeit/dao/ClassDAO.java
@@ -1,7 +1,6 @@
 package dao;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -31,7 +30,7 @@ public class ClassDAO {
 				conn.close();
 			}
 		} catch (SQLException e) {
-//			e.printStackTrace();
+			// e.printStackTrace();
 		}
 
 	}
@@ -40,8 +39,8 @@ public class ClassDAO {
 	public List<ClassVO> selectDetail(int classId) {
 		Connection conn = DBConnect.getInstance();
 		String sql = "SELECT CLASSID, CLASSNAME, CREATER, FAVORITE, OPENDATE FROM CLASS "
-				+ "WHERE CLASSID = (SELECT CLASSID FROM FAVORITE WHERE USERID = '" + id + "')"
-				+ "AND TYPE='P'";
+				+ "WHERE CLASSID = (SELECT CLASSID FROM FAVORITE WHERE USERID = '"
+				+ classId + "')" + "AND TYPE='P'";
 		Statement stmt = null;
 		ResultSet rs = null;
 		List<ClassVO> clist = new ArrayList<ClassVO>();
@@ -49,7 +48,7 @@ public class ClassDAO {
 		try {
 			stmt = conn.createStatement();
 			rs = stmt.executeQuery(sql);
-			
+
 			while (rs.next()) {
 				ClassVO cvo = new ClassVO();
 				cvo.setClassId(rs.getInt(1));
@@ -57,9 +56,9 @@ public class ClassDAO {
 				cvo.setCreater(rs.getString(3));
 				cvo.setFavorite(rs.getInt(4));
 				cvo.setOpenDate(rs.getDate(5));
-				
+
 				clist.add(cvo);
-				
+
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -69,24 +68,22 @@ public class ClassDAO {
 		}
 		return clist;
 	}
-	
-	
-	
-	//mypage -> select assignment class
-	public List<ClassVO> selectAssignmentClass(String id){
+
+	// mypage -> select assignment class
+	public List<ClassVO> selectAssignmentClass(String id) {
 		Connection conn = DBConnect.getInstance();
 		String sql = "SELECT CLASSNAME, CREATER, CLASSTYPE, RECOMMEND, CATEGORY FROM CLASS"
-					+ " WHERE CLASSID IN (SELECT CLASSID FROM ASSIGNMENT WHERE USERID = '" + id + "')"
-					+ " AND TYPE='O'"
-					+ " ORDER BY CATEGORY, CLASSID DESC";
+				+ " WHERE CLASSID IN (SELECT CLASSID FROM ASSIGNMENT WHERE USERID = '"
+				+ id + "')" + " AND TYPE='O'"
+				+ " ORDER BY CATEGORY, CLASSID DESC";
 		Statement stmt = null;
 		ResultSet rs = null;
 		List<ClassVO> clist = new ArrayList<ClassVO>();
-		
+
 		try {
 			stmt = conn.createStatement();
 			rs = stmt.executeQuery(sql);
-			
+
 			while (rs.next()) {
 				ClassVO cvo = new ClassVO();
 				cvo.setClassName(rs.getString(1));
@@ -94,9 +91,9 @@ public class ClassDAO {
 				cvo.setClassType(rs.getString(3));
 				cvo.setRecommend(rs.getInt(4));
 				cvo.setCategory(rs.getString(5));
-				
+
 				clist.add(cvo);
-				
+
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -106,31 +103,30 @@ public class ClassDAO {
 		}
 		return clist;
 	}
-	
-	//mypage -> select my class
-	//pre
-	public List<ClassVO> selectMyPreClass(String id){
+
+	// mypage -> select my class
+	// pre
+	public List<ClassVO> selectMyPreClass(String id) {
 		Connection conn = DBConnect.getInstance();
 		String sql = "SELECT CLASSNAME, CATEGORY, FAVORITE, OPENDATE FROM CLASS"
-				+ " WHERE CREATER = '" + id + "'"
-				+ " AND TYPE='P'";
+				+ " WHERE CREATER = '" + id + "'" + " AND TYPE='P'";
 		Statement stmt = null;
 		ResultSet rs = null;
 		List<ClassVO> clist = new ArrayList<ClassVO>();
-		
+
 		try {
 			stmt = conn.createStatement();
 			rs = stmt.executeQuery(sql);
-			
+
 			while (rs.next()) {
 				ClassVO cvo = new ClassVO();
 				cvo.setClassName(rs.getString(1));
 				cvo.setCategory(rs.getString(2));
 				cvo.setFavorite(rs.getInt(3));
 				cvo.setOpenDate(rs.getDate(4));
-				
+
 				clist.add(cvo);
-				
+
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -140,29 +136,28 @@ public class ClassDAO {
 		}
 		return clist;
 	}
-	//open
-	public List<ClassVO> selectMyOpenClass(String id){
+	// open
+	public List<ClassVO> selectMyOpenClass(String id) {
 		Connection conn = DBConnect.getInstance();
 		String sql = "SELECT CLASSNAME, CATEGRY, RECOMMEND, CLASSTYPE FROM CLASS"
-				+ " WHERE CREATER = '" + id + "'"
-				+ " AND TYPE='O'";
+				+ " WHERE CREATER = '" + id + "'" + " AND TYPE='O'";
 		Statement stmt = null;
 		ResultSet rs = null;
 		List<ClassVO> clist = new ArrayList<ClassVO>();
-		
+
 		try {
 			stmt = conn.createStatement();
 			rs = stmt.executeQuery(sql);
-			
+
 			while (rs.next()) {
 				ClassVO cvo = new ClassVO();
 				cvo.setClassName(rs.getString(1));
 				cvo.setCategory(rs.getString(2));
 				cvo.setRecommend(rs.getInt(3));
 				cvo.setClassType(rs.getString(4));
-				
+
 				clist.add(cvo);
-				
+
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -172,14 +167,15 @@ public class ClassDAO {
 		}
 		return clist;
 	}
-	
-	//create class
-	//classvo 확정되면 수정
-	public int insertClass(ClassVO cvo){
+
+	// create class
+	// classvo 확정되면 수정
+	public int insertClass(ClassVO cvo) {
 		Connection conn = DBConnect.getInstance();
 		StringBuilder sb = new StringBuilder();
-		//'부적합한 식별자'오류 발생
-		sb.append(" INSERT INTO CLASS ( CLASSID, CLASSNAME, CREATER, CLASSTYPE, PERIOD, " );
+		// '부적합한 식별자'오류 발생
+		sb.append(
+				" INSERT INTO CLASS ( CLASSID, CLASSNAME, CREATER, CLASSTYPE, PERIOD, ");
 		sb.append(" DETAIL, PRICE, CAPACITY, TYPE, CATEGORY, INTRODUCE ) ");
 		sb.append(" VALUES ( ( SELECT COUNT(*)+1 FROM CLASS ), '");
 		sb.append(cvo.getClassName());
@@ -208,11 +204,11 @@ public class ClassDAO {
 		int classid = 0;
 		try {
 			stmt = conn.createStatement();
-			//set prepared statement
-			
-			//get result
+			// set prepared statement
+
+			// get result
 			result = stmt.executeUpdate(sql);
-			if(result == 1) {
+			if (result == 1) {
 				classid = countClass();
 			}
 		} catch (SQLException e) {
@@ -222,8 +218,8 @@ public class ClassDAO {
 		}
 		return classid;
 	}
-	
-	//get classid
+
+	// get classid
 	public int countClass() {
 		Connection conn = DBConnect.getInstance();
 		String sql = " SELECT COUNT(*) FROM CLASS ";
@@ -232,10 +228,10 @@ public class ClassDAO {
 		int count = 0;
 		try {
 			stmt = conn.createStatement();
-			
-			//get result
+
+			// get result
 			rs = stmt.executeQuery(sql);
-			if(rs.next()) {
+			if (rs.next()) {
 				count = rs.getInt(1);
 			}
 		} catch (SQLException e) {
@@ -245,18 +241,19 @@ public class ClassDAO {
 		}
 		return count;
 	}
-	
-	//강의 페지
+
+	// 강의 페지
 	public int closeClass(int classid) {
 		Connection conn = DBConnect.getInstance();
-		//폐지상태로 변경
-		String sql = " UPDATE CLASS SET TYPE = 'C' WHERE CLASSID = '"+classid+"' ";
+		// 폐지상태로 변경
+		String sql = " UPDATE CLASS SET TYPE = 'C' WHERE CLASSID = '" + classid
+				+ "' ";
 		Statement stmt = null;
 		int result = 0;
 		try {
 			stmt = conn.createStatement();
-			
-			//get result
+
+			// get result
 			result = stmt.executeUpdate(sql);
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -265,43 +262,43 @@ public class ClassDAO {
 		}
 		return result;
 	}
-	
+
 	// detail page : class정보
-    public ClassVO getClassDetail(int classId) {
-        Connection conn = DBConnect.getInstance();
+	public ClassVO getClassDetail(int classId) {
+		Connection conn = DBConnect.getInstance();
 
-        String sql = "select classname, u.username, introduce, period, content_num, detail, chapter, creater_info, address "
-                + "from class c, webuser u "
-                + "where c.creater = u.userid and classid = " + classId;
+		String sql = "select classname, u.username, introduce, period, content_num, detail, chapter, creater_info, address "
+				+ "from class c, webuser u "
+				+ "where c.creater = u.userid and classid = " + classId;
 
-        Statement stmt = null;
-        ResultSet rs = null;
+		Statement stmt = null;
+		ResultSet rs = null;
 
-        ClassVO cvo = new ClassVO();
+		ClassVO cvo = new ClassVO();
 
-        try {
-            stmt = conn.createStatement();
-            rs = stmt.executeQuery(sql);
+		try {
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
 
-            while (rs.next()) {
-                cvo.setClassName(rs.getString(1));
-                cvo.setClassName(rs.getString(1));
-                cvo.setCreater(rs.getString(2));
-                cvo.setIntroduce(rs.getString(3));
-                cvo.setPeriod(rs.getInt(4));
-                cvo.setContent_num(rs.getInt(5));
-                cvo.setDetail(rs.getString(6));
-                cvo.setChapter(rs.getString(7));
-                cvo.setCreater_info(rs.getString(8));
-                cvo.setAddress(rs.getString(9));
-            }
+			while (rs.next()) {
+				cvo.setClassName(rs.getString(1));
+				cvo.setClassName(rs.getString(1));
+				cvo.setCreater(rs.getString(2));
+				cvo.setIntroduce(rs.getString(3));
+				cvo.setPeriod(rs.getInt(4));
+				cvo.setContent_num(rs.getInt(5));
+				cvo.setDetail(rs.getString(6));
+				cvo.setChapter(rs.getString(7));
+				cvo.setCreater_info(rs.getString(8));
+				cvo.setAddress(rs.getString(9));
+			}
 
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } finally {
-            closeAll(conn, null, stmt, rs);
-        }
-        return cvo;
-    }
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			closeAll(conn, null, stmt, rs);
+		}
+		return cvo;
+	}
 }
--- src/main/java/kr/re/kitri/takeit/dao/QnaDAO.java
+++ src/main/java/kr/re/kitri/takeit/dao/QnaDAO.java
@@ -12,8 +12,9 @@ import db.DBConnect;
 import vo.QnaVO;
 
 public class QnaDAO {
-	//closeAll
-	public void closeAll(Connection conn, PreparedStatement pstmt, Statement stmt, ResultSet rs) {
+	// closeAll
+	public void closeAll(Connection conn, PreparedStatement pstmt,
+			Statement stmt, ResultSet rs) {
 
 		try {
 			if (rs != null && !rs.isClosed()) {
@@ -33,24 +34,23 @@ public class QnaDAO {
 		}
 
 	}
-	
-	//help page -> Insert(qna등록)
-	
-	//mypage -> admin
-	//select
-	public List<QnaVO> selectAllQna(){
+
+	// help page -> Insert(qna등록)
+
+	// mypage -> admin
+	// select
+	public List<QnaVO> selectAllQna() {
 		Connection conn = DBConnect.getInstance();
-		String sql =" SELECT USERID, QNADATE, QUESTION, ANSWER, QNATITLE"
-				+ " FROM QNA"
-				+ " ORDER BY QNADATE DESC";
+		String sql = " SELECT USERID, QNADATE, QUESTION, ANSWER, QNATITLE"
+				+ " FROM QNA" + " ORDER BY QNADATE DESC";
 		Statement stmt = null;
 		ResultSet rs = null;
 		List<QnaVO> qlist = new ArrayList<QnaVO>();
-		
+
 		try {
 			stmt = conn.createStatement();
 			rs = stmt.executeQuery(sql);
-			
+
 			while (rs.next()) {
 				QnaVO qvo = new QnaVO();
 				qvo.setUserId(rs.getString(1));
@@ -58,55 +58,54 @@ public class QnaDAO {
 				qvo.setQuestion(rs.getString(3));
 				qvo.setAnswer(rs.getString(4));
 				qvo.setQnaTitle(rs.getString(5));
-				
+
 				qlist.add(qvo);
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
 			closeAll(conn, null, stmt, rs);
 		}
-		
+
 		return qlist;
 	}
-	//update
-	public int updateQna(String qnaTitle,String userId, String answer){
+	// update
+	public int updateQna(String qnaTitle, String userId, String answer) {
 		Connection conn = DBConnect.getInstance();
-		String sql ="UPDATE QNA SET ANSWER='" + answer + "'"
-				+ " WHERE QNATITLE='" + qnaTitle + "'"
-				+ " AND USERID='" + userId + "'";
+		String sql = "UPDATE QNA SET ANSWER='" + answer + "'"
+				+ " WHERE QNATITLE='" + qnaTitle + "'" + " AND USERID='"
+				+ userId + "'";
 		Statement stmt = null;
 		int result = 0;
 		try {
 			stmt = conn.createStatement();
-			
+
 			result = stmt.executeUpdate(sql);
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
 			closeAll(conn, null, stmt, null);
 		}
-		
+
 		return result;
 	}
-	
-	//mypage -> 일반 qna
+
+	// mypage -> 일반 qna
 	public List<QnaVO> selectMyQna(String id) {
 		Connection conn = DBConnect.getInstance();
 		String sql = "SELECT USERID, QNADATE, QUESTION, ANSWER, QNATITLE"
-					+ " FROM QNA"
-					+ " WHERE USERID ='" + id + "'"
-					+ " ORDER BY QNADATE DESC";
+				+ " FROM QNA" + " WHERE USERID ='" + id + "'"
+				+ " ORDER BY QNADATE DESC";
 		Statement stmt = null;
 		ResultSet rs = null;
 		List<QnaVO> qlist = new ArrayList<QnaVO>();
-		
+
 		try {
 			stmt = conn.createStatement();
 			rs = stmt.executeQuery(sql);
-			
+
 			while (rs.next()) {
 				QnaVO qvo = new QnaVO();
 				qvo.setUserId(rs.getString(1));
@@ -114,9 +113,9 @@ public class QnaDAO {
 				qvo.setQuestion(rs.getString(3));
 				qvo.setAnswer(rs.getString(4));
 				qvo.setQnaTitle(rs.getString(5));
-				
+
 				qlist.add(qvo);
-				
+
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -124,21 +123,20 @@ public class QnaDAO {
 		} finally {
 			closeAll(conn, null, stmt, rs);
 		}
-		
+
 		return qlist;
 	}
-	
-	//mypage -> Delete
+
+	// mypage -> Delete
 	public int deletQna(String userId) {
 		Connection conn = DBConnect.getInstance();
-		String sql = "DELETE FROM CLASSQNA"
-				+ " WHERE USERID = ?";
+		String sql = "DELETE FROM CLASSQNA" + " WHERE USERID = ?";
 		PreparedStatement pstmt = null;
 		int result = 0;
 		try {
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, userId);
-			
+
 			result = pstmt.executeUpdate();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -146,35 +144,32 @@ public class QnaDAO {
 		} finally {
 			closeAll(conn, pstmt, null, null);
 		}
-		
+
 		return result;
 	}
-	
-	public int qna(QnaVO qvo ) {
-        Connection conn = DBConnect.getInstance();
-        
-        String sql = "INSERT INTO QNA (USERID, QNADATE, QUESTION, QNATITLE)"
-                + "VALUES (?,sysdate,?,?)";
-        
-        PreparedStatement pstmt = null;
-        int result = 0;
-        try {
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, qvo.getUserid());
-            pstmt.setString(2, qvo.getQuestion());
-            pstmt.setString(3, qvo.getQnatitle());
-            
-            
-            result = pstmt.executeUpdate();
-        
-        
-        
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }finally {
-            closeAll(conn,pstmt,null,null);
-        }
-        return result;
-    }
+
+	public int qna(QnaVO qvo) {
+		Connection conn = DBConnect.getInstance();
+
+		String sql = "INSERT INTO QNA (USERID, QNADATE, QUESTION, QNATITLE)"
+				+ "VALUES (?,sysdate,?,?)";
+
+		PreparedStatement pstmt = null;
+		int result = 0;
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, qvo.getUserId());
+			pstmt.setString(2, qvo.getQuestion());
+			pstmt.setString(3, qvo.getQnaTitle());
+
+			result = pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			closeAll(conn, pstmt, null, null);
+		}
+		return result;
+	}
 }
