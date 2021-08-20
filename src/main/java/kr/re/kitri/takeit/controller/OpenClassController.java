e4fa438fb828a008b414b0d7e6e30ff28ad05517
horiJY <45135200+horiJY@users.noreply.github.com>
2021-08-20 09:41:26 +0900

    merged 지현

--- src/main/java/kr/re/kitri/takeit/controller/OpenClassController.java
+++ src/main/java/kr/re/kitri/takeit/controller/OpenClassController.java
@@ -1,8 +1,6 @@
 package controller;
 
 import java.io.IOException;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,67 +15,69 @@ import javax.servlet.http.HttpSession;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
-import vo.ClassVO;
-import vo.ScheduleVO;
 import dao.ClassDAO;
 import dao.ScheduleDAO;
+import vo.ClassVO;
+import vo.ScheduleVO;
 
 @SuppressWarnings("serial")
 @WebServlet("/mypage/openclass")
 public class OpenClassController extends HttpServlet {
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/open-class.jsp");
 		rd.forward(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//json 데이터 파싱
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// json 데이터 파싱
 		Gson gson = new Gson();
-		ClassVO cvo = gson.fromJson(request.getParameter("class"),ClassVO.class);
-		
-		JsonObject sJson = gson.fromJson(request.getParameter("schedule"),JsonObject.class);
+		ClassVO cvo = gson.fromJson(request.getParameter("class"), ClassVO.class);
+		cvo.setType("P");
+
+		JsonObject sJson = gson.fromJson(request.getParameter("schedule"), JsonObject.class);
 		JsonArray sJarr = sJson.get("classSchedule").getAsJsonArray();
 		List<ScheduleVO> slist = new ArrayList<ScheduleVO>();
-		for(int i=0;i<sJarr.size();i++) {
-			ScheduleVO svo = new Gson().fromJson(sJarr.get(i),ScheduleVO.class);
+		for (int i = 0; i < sJarr.size(); i++) {
+			ScheduleVO svo = new Gson().fromJson(sJarr.get(i), ScheduleVO.class);
 			slist.add(svo);
 		}
-		//이미 존재하는 스케쥴과 겹치지 않는 지 확인
+		// 이미 존재하는 스케쥴과 겹치지 않는 지 확인
 		HttpSession session = request.getSession();
-		String userId = (String)session.getAttribute("takeit-userid");
+		String userId = (String) session.getAttribute("takeit-userid");
 		ScheduleDAO sdao = new ScheduleDAO();
 		int result = 0;
-		for(int i=0;i<slist.size();i++){
+		for (int i = 0; i < slist.size(); i++) {
 			String stime = slist.get(i).getStartTime();
 			String etime = slist.get(i).getEndTime();
-			result = sdao.selectScheduleOverlap(userId,stime,etime);
-			if(result>0){
-				//중복 일정 존재
+			result = sdao.selectScheduleOverlap(userId, stime, etime);
+			if (result > 0) {
+				// 중복 일정 존재
 				response.getWriter().println("OVERLAP");
 				return;
 			}
 		}
 		ClassDAO cdao = new ClassDAO();
 		int classId = cdao.insertClass(cvo);
-		if(classId==0){
+		if (classId == 0) {
 			// 클래스 insert 실패
 			response.getWriter().println("FAIL");
 			return;
 		}
 
 		result = sdao.insertSchedule(slist);
-		if (result!=sJarr.size()){ 
+		if (result != sJarr.size()) {
 			// 앞에서 추가한 클래스 폐지
-			//	->update class' type to closed
+			// ->update class' type to closed
 			// 스케쥴 insert 실패 응답
 			response.getWriter().println("FAIL");
 			return;
 		}
-		//opendate 설정
-		
-		//강의 개설 성공 응답
+		// opendate 설정
+
+		// 강의 개설 성공 응답
 		response.getWriter().println("SUCCESS");
 	}
 }
